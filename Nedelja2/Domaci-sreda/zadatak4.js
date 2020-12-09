19accd17c75765568c9784d27785e5943c33e088
urkella <75279138+urkella@users.noreply.github.com>
2020-12-09 14:19:19 +0100

    Promena zadatak3.js za Nedelja2/Domaci-sreda

--- Nedelja2/Domaci-sreda/zadatak4.js
+++ Nedelja2/Domaci-sreda/zadatak4.js
@@ -2,7 +2,7 @@
 
 // Proci kroz svaki broj od 0 do 999 i
 // ispisati da li je deljiv sa brojem 5
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i <= 1000; i++) {
   var broj = 5; // Deljiv broj
 
   if (i % broj == 0) {
