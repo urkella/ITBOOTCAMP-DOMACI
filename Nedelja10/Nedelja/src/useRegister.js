72d051fcde099ec18c080f377506e1b1d239101c
urkella <urospavllovic@gmail.com>
2021-02-21 21:18:20 +0100

    Changes

--- Nedelja10/Nedelja/src/useRegister.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export const useRegister = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  return [username, setUsername, email, setEmail, password, setPassword];
-};
